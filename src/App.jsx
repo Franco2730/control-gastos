783c40fa59395edb3af17471c4e0ecbc97119db7
franco2730 <franco.rosales2130@gmail.com>
2024-02-20 16:45:24 -0300

    Control de gastos

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import NuevoPresupuesto from './components/NuevoPresupuesto'
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <NuevoPresupuesto/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Control de gastos</h1>
+    </header>
+  )
+}
+
+export default Header
